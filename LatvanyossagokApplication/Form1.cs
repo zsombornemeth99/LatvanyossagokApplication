9367c1c1309b6cbbd680eb642283af7a76579b33
Zsombor Németh <zsombor.nemeth99@gmail.com>
2020-12-05 12:34:40 +0100

    Induláskor táblák létrehozása, ha még nem léteznek

--- LatvanyossagokApplication/Form1.cs
+++ LatvanyossagokApplication/Form1.cs
@@ -19,7 +19,9 @@ namespace LatvanyossagokApplication
             InitializeComponent();
 
             kapcsolodas();
-            
+
+            tablakLetrehozasa();
+
             this.FormClosed += (sender, args) =>
             {
                 if (conn != null)
@@ -43,13 +45,43 @@ namespace LatvanyossagokApplication
             }
             catch (MySqlException ex)
             {
-                MessageBox.Show("Sikertelen kapcsolódás az adatbázishoz!");
+                MessageBox.Show("Sikertelen kapcsolódás az adatbázishoz!\nA program bezáródik!");
+                Environment.Exit(0);
             }
         }
 
         void tablakLetrehozasa()
         {
-
+            var varosok = @"CREATE TABLE IF NOT EXISTS varosok (
+                          id int(11) NOT NULL AUTO_INCREMENT,
+                          nev text COLLATE utf8mb4_hungarian_ci NOT NULL,
+                          lakossag int(11) NOT NULL,
+                          PRIMARY KEY (id),
+                          UNIQUE KEY nev (nev) USING HASH
+                          )";
+            var latvanyossagok = @"CREATE TABLE IF NOT EXISTS latvanyossagok (
+                                   id int(11) NOT NULL AUTO_INCREMENT,
+                                   varos_id int(11),
+                                   nev text COLLATE utf8mb4_hungarian_ci NOT NULL,
+                                   leiras text COLLATE utf8mb4_hungarian_ci NOT NULL,
+                                   ar int(11)  DEFAULT NULL,
+                                   PRIMARY KEY (id),
+                                   FOREIGN KEY (varos_id) REFERENCES varosok(id)
+                                   )";
+            var varosokComm = this.conn.CreateCommand();
+            var latvanyossagokComm = this.conn.CreateCommand();
+            varosokComm.CommandText = varosok;
+            latvanyossagokComm.CommandText = latvanyossagok;
+            try
+            {
+                varosokComm.ExecuteNonQuery();
+                latvanyossagokComm.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Hiba a tábla létrehozása közben!\n A program bezáródik!");
+                Environment.Exit(0);
+            }
         }
     }
 }
